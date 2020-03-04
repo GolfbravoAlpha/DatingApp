55885311faca410af60beb8ed0ba66486561aa1f
GolfbravoAlpha <mahi_rahman@outlook.com>
2020-03-04 17:01:21 +0000

    107 Section 11

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -44,6 +44,7 @@ namespace DatingApp.API
             });
             services.AddAutoMapper(typeof(DatingRepository).Assembly);
             services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
